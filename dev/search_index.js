b119d35b890cc7ef243d11edc6bb2a0b3fb63173
Documenter.jl <documenter@juliadocs.github.io>
2023-08-11 08:47:56 +0000

    build based on a55e332

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DryTooling","category":"page"},{"location":"#DryTooling","page":"Home","title":"DryTooling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DryTooling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DryTooling]","category":"page"}]
+}
