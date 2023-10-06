67904e2c669c6469374cde8c9941f4baf1f28b6d
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-10-06 12:07:28 +0200

    Progressing

--- devel/vertical/A1-Cantera-Support.py
+++ devel/vertical/A1-Cantera-Support.py
@@ -8,13 +8,17 @@ import cantera as ct
 import matplotlib.pyplot as plt
 import numpy as np
 
-P = 12500.0
-T = np.linspace(200, 3000, 100)
+# Both manometric pressure and temperature range will be shared across the mixtures.
 
+P = 12500.0
+T = np.linspace(400, 2800, 50)
+
+
+# Implementation of *pure substance* properties fitting is provided by `SubstanceFit`.
 
 class SubstanceFit:
-    """ Fit properties of mixture in temperature range. 
-    
+    """ Fit properties of mixture in temperature range.
+
     Parameters
     ----------
     T: list[float]
@@ -30,13 +34,13 @@ class SubstanceFit:
         Kinetics mechanism for creating solution.
     """
     def __init__(
-            self, 
+            self,
             T: list[float],
             P: float,
             Y: dict[str, float],
             degs: dict[str, int] = {
                 "cp_mass": 4,
-                "viscosity": 4, 
+                "viscosity": 4,
                 "thermal_conductivity": 4
             },
             mech: str = "gri30.yaml"
@@ -44,13 +48,14 @@ class SubstanceFit:
         gas = ct.Solution(mech)
         sol = ct.SolutionArray(gas, shape=T.shape)
         sol.TPY = T, ct.one_atm + P, Y
-    
+
         self._sol = sol
         self._mw = sol.mean_molecular_weight[1]
-        
+        self._Y = Y
+
         def fitrefs(p, d):
             return np.polyfit(sol.T, self._refs[p], deg=d)
-        
+
         self._refs = {p: getattr(sol, p) for p, d in degs.items()}
         self._fits = {p: fitrefs(p, d)   for p, d in degs.items()}
         self._coef = {
@@ -71,14 +76,14 @@ class SubstanceFit:
         cpr = self._refs["cp_mass"]
         mur = self._refs["viscosity"]
         kgr = self._refs["thermal_conductivity"]
-        
+
         cpf = np.polyval(self._fits["cp_mass"], T)
         muf = np.polyval(self._fits["viscosity"], T)
         kgf = np.polyval(self._fits["thermal_conductivity"], T)
-        
+
         plt.close("all")
         plt.style.use("seaborn-white")
-        fig, ax = plt.subplots(3, 1, figsize=(12, 9), sharex=True)
+        fig, ax = plt.subplots(3, 1, figsize=(10, 10), sharex=True)
 
         ax[0].plot(T, cpr, "k")
         ax[1].plot(T, mur, "k")
@@ -100,7 +105,13 @@ class SubstanceFit:
         ax[0].set_xticks(xtick)
 
         fig.tight_layout()
-        
+
+    def to_solution(self, T=1000, P=P):
+        """ Get simple solution from mixture. """
+        sol = ct.Solution(self._sol.source)
+        sol.TPY = T, P, self._Y
+        return sol
+
     @property
     def coefficients(self):
         """ Polynomial coefficients in decreasing order. """
@@ -130,31 +141,51 @@ def fluegases():
 mix2 = SubstanceFit(T, P, fluegases())
 mix2.plotsubstancefit()
 
+
 # ## Mixture law validation
 
-# +
-# def mixture(substances, masses):
-#     return sum(ct.Quantity(s._sol, mass=m) 
-#                for s, m in zip(substances, masses))
+class Mixture:
+    """ Mixture quantities of substances. """
+    def __init__(self, subs, T, Y):
+        """ Create a mixture of substances. """
+        if not np.allclose(sum(Y), 1.0):
+            raise ValueError(f"Mass fractions must add up to 1.0: {Y}")
 
-# +
-# substances = [mix0, mix1, mix2]
-# masses = [0.0, 0.0, 1.0]
-# mixture(substances, masses)
+        self._sol = [s.to_solution(T=t) for s, t in zip(subs, T)]
+        self._qty = [ct.Quantity(s, mass=y) for s, y in zip(self._sol, Y)]
 
-# +
-# q0 = ct.Quantity(mix0._sol[0], mass=0.1)
-# q1 = ct.Quantity(mix1._sol[0], mass=0.0)
-# q2 = ct.Quantity(mix2._sol[0], mass=0.9)
+        # Create a new object, Cantera is leaking (?!?!?)!
+        self._mix = ct.Quantity(self._sol[0], mass=Y[0])
 
-# +
-# gas = ct.Solution("gri30.yaml")
+        # Quantity is not compatible with `sum` (?!?!?!)!
+        for qk in self._qty[1:]:
+            self._mix += qk
 
-# +
-# ct.Quantity(gas, mass=0.1) + ct.Quantity(gas, mass=0.1)
-# -
+    @property
+    def mixture(self):
+        """ Access to internal mixture object. """
+        return self._mix
 
 
+# ### Case: isothermal mixture
+
+tmix = 1000
+mix = Mixture([mix0, mix1, mix2],
+              [tmix, tmix, tmix],
+              [ 0.2,  0.1,  0.7])
+
+sol = mix.mixture
+np.allclose(sol.cp_mass, sum([q.cp_mass * q.mass for q in mix._qty]))
+
+(sol.viscosity, sum([q.viscosity * q.mass for q in mix._qty]))
+
+(sol.thermal_conductivity, sum([q.thermal_conductivity * q.mass for q in mix._qty]))
+
+
+
+
+
+# ## Create database
 
 with open("mixtures.json", "w") as fp:
     json.dump({
@@ -162,3 +193,5 @@ with open("mixtures.json", "w") as fp:
         "mix1": mix1.coefficients,
         "mix2": mix2.coefficients
     }, fp, indent=4)
+
+
