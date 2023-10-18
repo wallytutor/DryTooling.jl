c4980ac34022983cfb436f112f3fe04727f15c01
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-10-18 05:18:36 +0200

    Organized stuff

--- src/DryTooling/grid-generation.jl
+++ src/DryTooling/grid-generation.jl
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-struct CylinderGrid1DEquispaced <: AbstractGrid1D
+struct RadialUserDefinedGrid1D <: AbstractGrid1D
     "Number of cells in domain."
     N::Int64
 
@@ -10,7 +10,23 @@ struct CylinderGrid1DEquispaced <: AbstractGrid1D
     "Radial coordinates of cells walls."
     w::Vector{Float64}
 
-    function CylinderGrid1DEquispaced(R::Float64, N::Int64)
+    function RadialUserDefinedGrid1D(r::Vector{Float64})
+        w = vcat(0.0, (r[2:end] + r[1:end-1])/2, last(r))
+        return new(length(r)-1, r, w)
+    end
+end
+
+struct RadialEquispacedGrid1D <: AbstractGrid1D
+    "Number of cells in domain."
+    N::Int64
+
+    "Radial coordinates of cells centers."
+    r::Vector{Float64}
+
+    "Radial coordinates of cells walls."
+    w::Vector{Float64}
+
+    function RadialEquispacedGrid1D(R::Float64, N::Int64)
         δr = R / N
         r = collect(0.0:δr:R)
         w = vcat(0.0, collect(0.5δr:δr:R-0.5δr), R)
@@ -18,5 +34,20 @@ struct CylinderGrid1DEquispaced <: AbstractGrid1D
     end
 end
 
-"Alias type for sphere equispaced discretization."
-const SphereGrid1DEquispaced = CylinderGrid1DEquispaced
+struct RadialGeometricGrid1D <: AbstractGrid1D
+    "Number of cells in domain."
+    N::Int64
+
+    "Radial coordinates of cells centers."
+    r::Vector{Float64}
+
+    "Radial coordinates of cells walls."
+    w::Vector{Float64}
+
+    function RadialGeometricGrid1D(R::Float64, N::Int64)
+        α = (R + 1)^(1 / N)
+        r = @. α^(0:N) - 1.0
+        w = vcat(0.0, (r[2:end] + r[1:end-1])/2, R)
+        return new(N, r, w)
+    end
+end
--- src/samples/diffusion-models.jl
+++ src/samples/diffusion-models.jl
@@ -4,27 +4,55 @@ Pkg.activate(Base.current_project())
 Pkg.instantiate()
 
 using Revise
-import DryTooling as dry
+using CairoMakie
+using DryTooling: RadialUserDefinedGrid1D
+using DryTooling: RadialEquispacedGrid1D
+using DryTooling: RadialGeometricGrid1D
+using DryTooling: Cylinder1DTemperatureModel
+using DryTooling: solve
 
-R = 0.05
-tol = 1.0e-10
+function illustrategridconvergence()
+    N = 100
+    R = 0.05
+    c = 900.0
+    ρ = 3000.0
+    h = (t) -> (t < 2000) ? 20000.0 : 0.0
+    B = (t) -> 1500.0 + 200sin(2π * t / 240)
+    κ = (T) -> 2.0 + 0.0001T
 
-grid = dry.CylinderGrid1DEquispaced(R, 100)
+    grids = [
+        RadialEquispacedGrid1D(R, N),
+        RadialGeometricGrid1D(R, N),
+        RadialUserDefinedGrid1D(geomspace(0, R, 0.003, 0.0005))
+    ]
 
-h = (t) -> (t < 2300) ? 20.0 : 0.0
-B = 1500.0
-κ = 2.0
-ρ = 3000.0
-c = 900.0
+    fig = Figure(resolution = (720, 500))
+    ax = Axis(fig[1, 1], yscale = identity)
 
-model = dry.Cylinder1DTemperatureModel(grid, h, B, κ, ρ, c)
+    for grid in grids
+        f = typeof(grid)
+        model = Cylinder1DTemperatureModel(grid, h, B, κ, ρ, c)
 
-@time residuals = dry.solve(model;
-    T     = 300.0,
-    τ     = 2.0,
-    t     = 2400.0,
-    iters = 20,
-    relax = 0.0001,
-    tol   = tol,
-    metric = dry.maxrelativevariation
-)
+        @time r = solve(model;
+            T     = 300.0,
+            τ     = 2.0,
+            t     = 2400.0,
+            iters = 20,
+            relax = 0.001,
+            tol   = 1.0e-10
+        )
+
+        lines!(ax, 100grid.r, model.problem.x,
+            label = "$(f) | N = $(grid.N)")
+    end
+
+    ax.xlabel = "Radial coordinate [cm]"
+    ax.ylabel = "Temperature [K]"
+    ax.xticks = 0.0:1.0:100R
+    xlims!(ax, (-0.2, 100R+0.2))
+    axislegend(ax; position = :lt)
+    fig
+end
+
+fig = illustrategridconvergence()
+
